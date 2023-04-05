1de55d7393cfd510a61551ec328221050d5b8c3a
AmmarJ23 <ammar.jmldn@outlook.com>
2023-04-05 11:13:45 +0800

    Allen?

--- week03/src/Address.java
+++ week03/src/Address.java
@@ -2,23 +2,24 @@ public class Address {
     int homeNum;
     String streetAddress;
 
-    public Address(int hN, String sA)
-    {
-        this.homeNum = hN;
-        this.streetAddress = sA;
-    }
-
     public Address()
     {
         homeNum = 0;
         streetAddress = null;
     }
 
+    public Address(int hN, String sA)
+    {
+        this.homeNum = hN;
+        this.streetAddress = sA;
+    }
+
     public void setHomeNum(int hN) {this.homeNum = hN;}
     public void setStreetAddress(String sA){this.streetAddress = sA;}
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "\nHome Number: " + homeNum + "\nStreet Address: " + streetAddress;
     }
 }
--- week03/src/Person.java
+++ week03/src/Person.java
@@ -1,4 +1,5 @@
-public class Person {
+public class Person 
+{
     String name;
     int yob;
     float weight, height, bmi;
@@ -10,7 +11,8 @@ public class Person {
     }
 
 
-    public Person(String n, int a, float w, float h, Address addr) {
+    public Person(String n, int a, float w, float h, Address addr) 
+    {
         this.name = n;
         this.yob = a;
         this.weight = w;
@@ -30,60 +32,71 @@ public class Person {
     }
 
 
-    public void calcBmi() {
+    public void calcBmi() 
+    {
         bmi = weight / (height * height);
     }
 
 
     // all setters /mutators (4)
-    public void setName(String n) {
+    public void setName(String n) 
+    {
         this.name = n;
     }
 
 
-    public void setYob(int y) {
+    public void setYob(int y) 
+    {
         this.yob = y;
     }
 
 
-    public void setWeight(float w) {
+    public void setWeight(float w) 
+    {
         this.weight = w;
     }
 
 
-    public void setHeight(float w) {
+    public void setHeight(float w) 
+    {
         this.weight = w;
     }
 
 
     // all getters /accesors (5)
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
 
-    public int getYob() {
+    public int getYob() 
+    {
         return this.yob;
     }
 
 
-    public float getWeight() {
+    public float getWeight() 
+    {
         return this.weight;
     }
 
 
-    public float getHeight() {
+    public float getHeight() 
+    {
         return this.height;
     }
 
 
-    public float getBmi() {
+    public float getBmi() 
+    {
         return bmi;
     }
 
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Person info : \nname = " + this.name + " yob : " + this.yob + " \nweight : " + this.weight
                 + " height : " + this.height + " \nbmi : " + this.bmi + this.addr;
     }
