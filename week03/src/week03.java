cf31cd368a72067bdd260331fd6218bfb566b5a6
AmmarJ23 <ammar.jmldn@outlook.com>
2023-04-05 11:17:15 +0800

    Update

--- week03/src/week03.java
+++ week03/src/week03.java
@@ -1,6 +1,6 @@
 public class week03 {
     public static void main(String[] args) {
-        System.out.println("Week03");
+        System.out.println("\nWeek03");
         Address addr = new Address(3, "Maple St.");
         Person p1 = new Person("ali", 1999, 99, 1.99f, addr);
         Person p2 = new Person("Abu", 2000, 66, 1.66f, 23, "Jalan Kilang");
@@ -8,7 +8,7 @@ public class week03 {
         p1.calcBmi();
         p2.calcBmi();
         System.out.println(p1.toString());
-        System.out.println("\n\n");
+        System.out.println("\n");
         System.out.println(p2.toString());
         System.exit(0);
     }
