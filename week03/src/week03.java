2faf77d9f4f430b241896f32ec9c30b91c9fdc05
AmmarJ23 <ammar.jmldn@outlook.com>
2023-04-05 10:53:58 +0800

    Initial Commit

---
+++ week03/src/week03.java
@@ -0,0 +1,11 @@
+public class week03 {
+    public static void main(String[] args) {
+        System.out.println("Week03");
+        Address addr = new Address("No1")
+        Person p = new Person("ali", 1999, 99, 1.99f);
+        p.calcBmi();
+        System.out.println(p.toString());
+        System.exit(0);
+    }
+
+}
