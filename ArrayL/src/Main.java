ecbb90dd75e5a66e5e1aa02183bd5fea4de32b4b
AmmarJ23 <ammar.jmldn@outlook.com>
2023-04-10 08:38:29 +0800

    c

--- ArrayL/src/Main.java
+++ ArrayL/src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
         int[] intArray;
         intArray = new int[10];
 
         for (int i = 0; i < 10; i++) intArray[i] = i;
 
-        for (int i = 0; i < 10; i++) System.out.println(intArray[i] + "\n");
-      
+        for (int i = 0; i < 10; i++) System.out.println(intArray[i]);
+
+        for (int i = 0; i < intArray.length; i++) intArray[i] = intArray[i] + 10;
+
+        for (int i = 0; i < 10; i++) System.out.println(intArray[i]);
+
+        float fNum[] = {35f, 10f, 13f, 14f, 15f, 19f};
+
+        System.out.println(fNum[4]);
+
+
     }
 }
