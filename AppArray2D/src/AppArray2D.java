17ed4749b422d3601b028fd8024a9f67e17608f4
AmmarJ23 <ammar.jmldn@outlook.com>
2023-04-12 11:01:42 +0800

    Task 2,3,4

--- AppArray2D/src/AppArray2D.java
+++ AppArray2D/src/AppArray2D.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AppArray2D {
 
     public static void main(String[] args) {
@@ -15,5 +17,56 @@ public class AppArray2D {
         for (double cm : courseMark) {
             System.out.println(cm);
         }
+
+        // double[][] cmas = new double [3][5];
+        // cmas[0][0] = 4.5;
+        // cmas[0][1] = 4.0;
+        // cmas[0][2] = 13.5;
+        // cmas[0][3] = 9.3;
+        // cmas[0][4] = 3.7;
+
+        // cmas[1][0] = 8.2;
+        // cmas[1][1] = 14.1;
+        // cmas[1][2] = 2.5;
+        // cmas[1][3] = 18.9;
+        // cmas[1][4] = 6.7;
+
+        // cmas[2][0] = 5.6;
+        // cmas[2][1] = 11.8;
+        // cmas[2][2] = 19.4;
+        // cmas[2][3] = 1.9;
+        // cmas[2][4] = 13.2;
+
+        //Task 2: assign value to 2D array using shorthand notation
+        double[][] cmas = {
+            {4.5, 4.0, 13.5, 9.3, 3.7},
+            {8.2, 14.1, 2.5, 18.9, 6.7},
+            {5.6, 11.8, 19.4, 1.9, 13.2}
+        };
+
+        //Task3: for loop to output array
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("["+i+"]["+j+"]: " + cmas[i][j]);
+            }
+        }
+
+        //Task 4: for loop to ask for user input
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter double values:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                cmas[i][j] = s.nextDouble();
+            }
+        }
+
+        //for loop to output array
+        System.out.println("Content of cmas 2D array");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("["+i+"]["+j+"]: " + cmas[i][j]);
+            }
+        }
+        s.close();
     }
 }
