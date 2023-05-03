52a2418b340de2a8673cdeafeda6466ecc3659a9
AmmarJ23 <ammar.jmldn@outlook.com>
2023-05-03 10:41:10 +0800

    jnyy

--- week7/src/App.java
+++ week7/src/App.java
@@ -3,12 +3,16 @@ import java.util.Vector;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Vector vname = new Vector();
-        vname.add("abu");
+        Vector <String> vname = new Vector <String> ();
         vname.add("ali");
+        vname.add("abu");
         vname.add("siti");
 
         vname.add(1, "alice");
+        vname.set(2, "bob");
+
+        vname.remove(1);
+
 
         System.out.println("vname size: " + vname.size()); //print current vector size
         System.out.println(vname); //print all elements
