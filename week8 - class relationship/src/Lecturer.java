c862eabcd74f8f8808f1b24376d22a4327a7e161
AmmarJ23 <ammar.jmldn@outlook.com>
2023-05-10 10:40:18 +0800

    Init

---
+++ week8 - class relationship/src/Lecturer.java
@@ -0,0 +1,17 @@
+public class Lecturer {
+    private String staffNum;
+
+    public Lecturer(){}
+    public Lecturer(String staffNum)
+    {
+        this.staffNum = staffNum;
+    }
+
+    public String getStaffNum() {
+        return staffNum;
+    }
+
+    public void setStaffNum(String staffNum) {
+        this.staffNum = staffNum;
+    }
+}
