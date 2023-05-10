f392386a4542ac407acfb9fb43a78eb1703d6a4d
AmmarJ23 <ammar.jmldn@outlook.com>
2023-05-10 11:48:46 +0800

    dwqdqdqwdqw

--- week8 - class relationships - composition/src/Car.java
+++ week8 - class relationships - composition/src/Car.java
@@ -3,15 +3,20 @@ public class Car {
     private Engine engine;
     private Wheel[] wheels;
 
+
     private String make;
     private String model;
 
 
     //constructor
-    public Car(String make, String model) {
-        chassis = new Chassis();
-        engine =  new Engine();
-        wheels =  new Wheel[5];
+    public Car(String make, String model, Chassis c, Engine e, Wheel w[]) {
+        // chassis = new Chassis();
+        // engine =  new Engine();
+        // wheels =  new Wheel[5];
+
+        chassis = c;
+        engine = e;
+        wheels = w;
 
         this.make = make;
         this.model = model;
